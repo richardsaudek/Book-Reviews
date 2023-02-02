02eb4f321be5bc2119cfbfa832c15a2002d3a9cc
richardsaudek <richard.saudek@gmail.com>
2023-02-02 13:56:39 -0500

    results are showing up

--- script.js
+++ script.js
@@ -1,45 +1,75 @@
 const DOMAIN = 'https://api.nytimes.com/svc/books/v3/reviews.json?';
 const QUERY = "api-key=E1a7bAdoiHcGemouHZDyd8bfEwS3tUNd"
+// const throttledApi = _.throttle(api, 1000)
+// throttledApi();
 
 const button = document.querySelector("button")
+//let theText = <input>("")</input>;
+const span = document.querySelector("span")
 const listing = document.querySelector(".book-list")
- 
+const test = document.querySelector("#test")
+console.log(test);
+const output = document.querySelector('#output')
 
-async function getAuthor(book_author) {
- 
-  const url = `${DOMAIN}author=${book_author}&${QUERY}`;
-
-  try {
-    const res = await fetch(`${url}`)
-    const json = await res.json()
-    console.log(json);
-    return json;
-  } catch (error) {
+button.addEventListener("click", () => {
+  const bookTitle = document.getElementById("input").value;
+  getAuthor(bookTitle)
   
-  }
+})
+  async function getAuthor(book_author) {
+    const url = `${DOMAIN}author=${book_author}&${QUERY}`;
+    try {
+      const results = await fetch(`${url}`)
+      const json = await results.json()
+      console.log(json);
+for (let index = 0; index < json.results.length; index++) {
+  
+  const authorinfo = document.createElement("div")
+  // authorinfo.textContent = json.results[index].book_author
+  output.appendChild(authorinfo)
+  
+  const titleinfo = document.createElement("div")
+  titleinfo.textContent = json.results[index].book_title
+  output.appendChild(titleinfo)
+  
+  const summaryinfo = document.createElement("div")
+  summaryinfo.textContent = json.results[index].summary
+  output.appendChild(summaryinfo)
+
+  const urlinfo = document.createElement("div")
+  urlinfo.textContent = json.results[index].url
+  output.appendChild(urlinfo)
+
+
+  span.innerHTML = json.results[index].book_author
+
+  
 }
-getAuthor('stephen king')
 
-  // async function
-  //   const res = await 'https://api.nytimes.com/svc/books/v3/reviews.json'
-  // const json = res.json()
-  //   return json
-
-  // async function api(author) {
-  //   const url = `${DOMAIN}/reviews.json${QUERY}`;
-  //   const res = await fetch(url)
-  //   const json = await res.json()
-  //   console.log(`This is the result of the API function`);
-  //   console.log(json);
-  //   return json
-  //   console.log('Haruki Murakami');
-  // }
-//   const throttledApi = _.throttle(api, 1000)
-//   throttledApi();
   
-//   button.addEventListener("click"), () => {
-//     const bookList = document.getElementById("blank").value;
-//     searchApi(bookList)
-//   }
-// }
+    }
+    catch (error) {
+    }
+  }
   // async function renderList(json) {
+  //   listing.innerHTML = ""
+  //   for (let book of json.Search) {
+  //     let Title = book.Title;
+  //     let Author = book.Author
+  //     let NYTimesReview = book.Review
+  //     let Summary = book.Summary
+    
+  //     const div = document.createElement('div');
+  //     const titleBox = document.createElement('p');
+  //     const img = document.createElement(`img`)
+  
+  //     div.classList.add("bookBox")
+  //     img.classList.add('img')
+  //     titleBox.classList.add("titleBox")
+
+  //     // getAuthor('Haruki Murakami')
+  //   }
+  //   renderList(json)
+  // }
+
+
