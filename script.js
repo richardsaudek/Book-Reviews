f3ae48f2084c3ec67487eadbd7ee23a0fd217526
richardsaudek <richard.saudek@gmail.com>
2023-02-03 12:06:25 -0500

    found images

--- script.js
+++ script.js
@@ -1,56 +1,87 @@
-const DOMAIN = 'https://api.nytimes.com/svc/books/v3/reviews.json?';
-const QUERY = "api-key=E1a7bAdoiHcGemouHZDyd8bfEwS3tUNd"
-const OVERVIEW_API = '/lists/overview.json?';
-const IMAGE_API = 'list_image'
-
 // const throttledApi = _.throttle(api, 1000)
 // throttledApi();
+const DOMAIN = 'https://api.nytimes.com/svc/books/v3/reviews.json?';
+const QUERY = "api-key=E1a7bAdoiHcGemouHZDyd8bfEwS3tUNd"
+const OVERVIEW_API = 'https://api.nytimes.com/svc/books/v3/lists/overview.json';
+
 const button = document.querySelector("button")
-//let theText = <input>("")</input>;
 const span1 = document.querySelector("span1")
 const listing = document.querySelector(".book-list")
-const test = document.querySelector("#test")
-console.log(test);
 const output = document.querySelector('#output')
+// const isbn = document.querySelector()
+
 
 button.addEventListener("click", () => {
   const bookTitle = document.getElementById("input").value;
   getAuthor(bookTitle)
   
 })
-async function getBookImage(list_image)
-const urlImage = `${DOMAIN}author=${book_author}&${QUERY}&${OVERVIEW_API}&{IMAGE_API}`;
-try {
-  const results = await fetch(`${urlImage}`)
-  const json = await results.json()
-  console.log(json);
-}
-for (let index = 0; index < 10; index++) {
-const bookbox = document.createElement("div");
-bookbox.classList.add("bookBox");
-  async function getAuthor(book_author) {
-    const url = `${DOMAIN}author=${book_author}&${QUERY}`;
-    try {
-      const results = await fetch(`${url}`)
-      const json = await results.json()
-      console.log(json);
-    }
-      
-for (let index = 0; index < 10; index++) {
-  const bookbox = document.createElement("div");
-  bookbox.classList.add("bookBox");
+// async function getBookImage(list_image)
+// const image = `${DOMAIN}image=${list_image}&${QUERY}`;
+// try {
+//   const results = await fetch(`${urlImage}`)
+//   const json = await results.json()
+//   console.log(json);
+// }
+// for (let index = 0; index < 10; index++) {
+// const bookbox = document.createElement("div");
+// bookbox.classList.add("bookBox");
   
-  const titleinfo = document.createElement("div")
-  titleinfo.textContent = json.results[index].book_title
-  bookbox.appendChild(titleinfo)
+// function updateCover(id, isbn) {
+//   fetch('https://www.googleapis.com/books/v1/volumes?q=isbn:' + isbn + "&key=AIzaSyAyINR2SYnt4K-0x6zh6S3x6NVUY15pY7Q", {
+//     method: 'get'
+//   })
+//   .then(response => { return response.json(); })
+//   .then(data => {
+//     var img = data.items[0].volumeInfo.imageLinks.thumbnail;
+//     img = img.replace(/^http:\/\//i, 'https://');
+//     $('#cover-' + id).attr('src', img);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//     // console.log('Googel API Error: Defaulting to archival images for book #' + id + ' ISBN: ' + isbn);
+//     // var index = id - 1;
+//     // var img = archivedImages[index];
+//     // $('#cover-' + id).attr('src', img);
+//   });
+// }
 
-  const url = document.createElement("div")
-  url.textContent = json.results[index].url
-  bookbox.appendChild(url)
-
-  const summaryinfo = document.createElement("div")
-  summaryinfo.textContent = json.results[index].summary
-  bookbox.appendChild(summaryinfo)
+async function getAuthor(book_author) {
+  const url = `${DOMAIN}author=${book_author}&${QUERY}`;
+  try {
+    const results = await fetch(`${url}`)
+    const json = await results.json()
+    const imagebox = document.querySelector('.something')
+    for (let index = 0; index < 10; index++) {
+      const isbn13 = json.results[index].isbn13[0]
+      const img = document.createElement('img')
+      img.src = `https://pictures.abebooks.com/isbn/${isbn13}-us-300.jpg`
+      imagebox.append(img)
+      const bookbox = document.createElement("div");
+      bookbox.classList.add("bookBox");
+      
+      const titleinfo = document.createElement("div")
+      titleinfo.textContent = json.results[index].book_title
+      bookbox.appendChild(titleinfo)
+    
+      const url = document.createElement("div")
+      url.textContent = json.results[index].url
+      bookbox.appendChild(url)
+    
+      const summaryinfo = document.createElement("div")
+      summaryinfo.textContent = json.results[index].summary
+      bookbox.appendChild(summaryinfo)
+      
+      console.log(json);
+      output.appendChild(bookbox)
+    
+      span1.innerHTML = json.results[index].book_author
+      updateCover()
+    }
+  }
+  catch (error) {
+  }
+}
   
   // const authorinfo = document.createElement("div")
   // // authorinfo.textContent = json.results[index].book_author
@@ -62,27 +93,20 @@ for (let index = 0; index < 10; index++) {
   // urlinfo.textContent = json.results[index].url
   // bookbox.appendChild(urlinfo)
 
-  output.appendChild(bookbox)
 
-  span1.innerHTML = json.results[index].book_author
-} 
-    }
-    catch (error) {
-    }
     
-    }
-function renderList(json) {
-  listing.innerHTML = ""
-  for (let movie of json.Search) {
-    let Title = titleinfo;
-    let Summary = summaryinfo
-    let Link = url
+// function renderList(json) {
+//   listing.innerHTML = ""
+//   for (let movie of json.Search) {
+//     let Title = titleinfo;
+//     let Summary = summaryinfo
+//     let Link = url
       
-    const div = document.createElement('div');
-    const titleBox = document.createElement('p');
-    const img = document.createElement(`img`)
-  }
-}
+//     const div = document.createElement('div');
+//     const titleBox = document.createElement('p');
+//     const img = document.createElement(`img`)
+//   }
+// }
 
     // div.classList.add("bookBox")
     // img.classList.add('url')
